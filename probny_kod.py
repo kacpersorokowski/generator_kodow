b5ffa9b7349487024d1e5c25dbaff0664396deb0
kacpersorokowski <kacper.sorokowski@gmail.com>
2026-08-29 23:02:09 +0200

    Add files via upload

---
+++ probny_kod.py
@@ -0,0 +1,100 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+
+    kod = "Tu pojawi się kod"
+
+    dane = {
+        "grupa": "",
+        "producent": "",
+        "kompatybilnosc": "",
+        "rodzaj": "",
+        "kolor": "",
+        "pochodzenie": "",
+        "numer": "1"
+    }
+
+    if request.method == "POST":
+
+        dane["grupa"] = request.form.get("grupa", "")
+        dane["producent"] = request.form.get("producent", "")
+        dane["kompatybilnosc"] = request.form.get("kompatybilnosc", "")
+        dane["rodzaj"] = request.form.get("rodzaj", "")
+        dane["kolor"] = request.form.get("kolor", "")
+        dane["pochodzenie"] = request.form.get("pochodzenie", "")
+        dane["numer"] = request.form.get("numer", "1")
+
+        kodzik = ""
+
+        # GRUPA
+        if dane["grupa"] == "Materiały eksploatacyjne":
+            kodzik += "ME"
+
+        # PRODUCENT
+        producenci = {
+            "Hewlett Packard": "HP",
+            "Brother": "BR",
+            "Canon": "CA",
+            "Epson": "EP"
+        }
+
+        kodzik += producenci.get(dane["producent"], "")
+
+        # KOMPATYBILNOŚĆ
+        kodzik += producenci.get(dane["kompatybilnosc"], "")
+
+        # RODZAJ
+        rodzaje = {
+            "Tusz": "INK",
+            "Toner": "TON",
+            "Bębeb": "BB"
+        }
+
+        kodzik += rodzaje.get(dane["rodzaj"], "")
+
+        # KOLOR
+        kolory = {
+            "Czarny": "BK",
+            "Niebieski": "C",
+            "Różowy": "M",
+            "Żółty": "Y",
+            "Kolorowy": "CMY",
+            "Zestaw": "CMYK"
+        }
+
+        kodzik += kolory.get(dane["kolor"], "")
+
+        # POCHODZENIE
+        pochodzenie = {
+            "Dystrybucja": "D",
+            "Rynek": "R"
+        }
+
+        kodzik += pochodzenie.get(dane["pochodzenie"], "")
+
+        # NUMER
+        try:
+            numer = int(dane["numer"])
+
+            kodzik += str(numer)
+
+            dane["numer"] = str(numer + 1)
+
+        except ValueError:
+            kodzik = "Wpisz poprawny numer!"
+
+        kod = kodzik
+
+    return render_template(
+        "index.html",
+        kod=kod,
+        dane=dane
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
